594667bcb557ba7c176aeba06c86f00c4e691c97
Daniel Berg <mail@roosta.sh>
2016-05-25 12:48:36 +0200

    Create ~/bin and symlink fatline.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/env bash
 sudo pacman -S ansible && \
   mkdir ~/.ansible && \
+  mkdir ~/bin && \
   ln -s ~/.dotfiles/ansible/.ansible.cfg ~ && \
   ln -s ~/.dotfiles/ansible/hosts ~/.ansible && \
-  ln -s ~/.dotfiles/ansible/library ~/.ansible
+  ln -s ~/.dotfiles/ansible/library ~/.ansible && \
+  ln -s ~/.fatline/scripts/fatline.sh ~/bin/fatline
 
