58bddcdbb30326696cafaa31e505a1bfeae0135d
Daniel Berg <mail@roosta.sh>
2016-05-30 22:33:59 +0200

    Fix bootstrap package install.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
-mkdir ~/.ansible && \
+packages() {
+  if hash pacman 2>/dev/null; then
+    sudo pacman -S ansible
+  elif hash apt-get 2>/dev/null; then
+    sudo apt-get install ansible
+  else
+    echo "No package manager found. Install ansible manually."
+  fi
+}
+
+
+packages && \
+  mkdir ~/.ansible && \
   mkdir ~/bin && \
   ln -s ~/.dotfiles/ansible/.ansible.cfg ~ && \
   ln -s ~/.dotfiles/ansible/hosts ~/.ansible && \
